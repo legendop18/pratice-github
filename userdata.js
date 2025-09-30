6640e8390d1e9c2bf30cf1710e25fd3e27a63554
Ajinkya pandit <ajinkyapandit098@gmail.comm>
2025-09-30 19:59:57 +0530

    user-data

---
+++ userdata.js
@@ -0,0 +1,7 @@
+const users = [
+  { id: 1, name: "Rocky", email: "rocky@example.com", age: 22 },
+  { id: 2, name: "Ajinkya", email: "ajinkya@example.com", age: 25 },
+  { id: 3, name: "Sneha", email: "sneha@example.com", age: 28 },
+  { id: 4, name: "Rahul", email: "rahul@example.com", age: 21 },
+  { id: 5, name: "Priya", email: "priya@example.com", age: 30 },
+];
