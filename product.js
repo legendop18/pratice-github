eb90faa223ce6ec5e264e7c1d3bfbafff11925de
Ajinkya pandit <ajinkyapandit098@gmail.comm>
2025-09-30 20:06:17 +0530

    product data

---
+++ product.js
@@ -0,0 +1,7 @@
+const products = [
+  { id: 101, name: "Laptop", price: 60000, category: "Electronics" },
+  { id: 102, name: "Mobile", price: 25000, category: "Electronics" },
+  { id: 103, name: "Headphones", price: 2000, category: "Accessories" },
+  { id: 104, name: "Shoes", price: 3000, category: "Fashion" },
+  { id: 105, name: "Book - JavaScript", price: 500, category: "Books" },
+];
